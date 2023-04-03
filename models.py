ce0e1ae99f53d2d29d5e228332f6968e24ff1854
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-04-04 01:38:30 +0300

    initial constructor

--- models.py
+++ models.py
@@ -10,11 +10,20 @@ class Notes:
         1 — note title
         2 — note text
         '''
-        self.note_id = args[0]
-        self.title = args[1]
-        self.text = args[2]
-        self.date = datetime.date.today()
-        self.time = datetime.datetime.now().time()
+        if len(args) == 3:
+            self.note_id = args[0] + 1
+            self.title = args[1]
+            self.text = args[2]
+            self.date = datetime.date.today()
+            self.time = datetime.datetime.now().time()
+        elif len(args[0]) == 5:
+            self.note_id = int(args[0][0])
+            self.title = args[0][1]
+            self.text = args[0][2]
+            self.date = datetime.datetime.strptime(args[0][3], '%d.%m.%Y').date()
+            self.time = datetime.time.fromisoformat(args[0][4])
+        else:
+            print('error')
 
     def __repr__(self):
         return str(self.note_id) + ' ' + datetime.datetime.strftime(self.date,
@@ -23,3 +32,9 @@ class Notes:
     def __str__(self):
         return str(self.note_id) + ' ' + datetime.datetime.strftime(self.date,
                                                                     '%d.%m.%Y') + ' ' + self.title + ' ' + self.text
+
+    def note_edit(self, title, text):
+        self.title = title
+        self.text = text
+        self.date = datetime.date.today()
+        self.time = datetime.datetime.now().time()
