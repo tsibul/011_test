8fbed8b6de47197e405031833d1176e51911d564
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-04-04 01:39:29 +0300

    menu dicts

---
+++ menu.py
@@ -0,0 +1,18 @@
+main_menu = {
+    '1': 'View notes',
+    '2': 'Edit notes',
+    '0': 'Exit'
+}
+
+view_menu = {
+    '1': 'View all',
+    '2': 'Filter by date',
+    '0': 'Return to Main menu'
+}
+
+edit_menu = {
+    '1': 'Add record',
+    '2': 'Edit record',
+    '3': 'Delete record',
+    '0': 'Return to Main menu'
+}
